2b82a8c574241c74be47e97a912f125bdd767d93
2alheure <jojo77.juv@live.fr>
2020-12-08 14:53:07 +0100

    init

---
+++ PHP7/P2-A-III.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Je m'appelle Jordan JUVENTIN 
+ * Et j'adore le PHP
+ */
+
+// Vous remarquerez que je préfère les commentaires en bloc
+// Nan parce que sinon ça devient vite embêtant
+// D'écrire sur plusieurs lignes
+?>
+
+// Attention, ceci n'est pas un commentaire !
+<!-- Mais ceci en est un ;-) 
+(Un commentaire en HTML, pour le coup) -->
+
+<?php 
+<!-- Et ceci n'en est plus un... -->
+
+// Tandis qu'ici maintenant oui =)
+
+/**
+ * Pourquoi ? 
+ * 
+ * Observez les balises <?php et ?>
+ * Ce sont elles qui délimitent le code PHP
+ */
+
+ // Dernière particularité :
+ // Pas besoin de balise fermante à la fin d'un fichier PHP s'il se termine par du PHP ;-)
---
+++ PHP7/P2-B-I.php
@@ -0,0 +1,16 @@
+<?php
+
+// Déclarer une variable
+
+
+// Affecter un nombre à cette variable
+
+
+// Affecter une chaîne de caractères à cette variable
+
+
+// Affecter un tableau vide à cette variable
+
+
+// Affecter un booléen à cette variable
+
