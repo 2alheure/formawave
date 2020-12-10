037b741fa82c88069de4bb00a003e3220ae85915
2alheure <jojo77.juv@live.fr>
2020-12-10 18:41:28 +0100

    Exercices sur les chaînes de caractères

--- PHP7/P2/B/P2-B-III-b.php
+++ PHP7/P2/B/P2-B-III-b.php
@@ -1,6 +1,56 @@
 <?php
+
 /********* Partie 2 ****************************/
 /********* B. Les instructions de base *********/
 /********* III. Les chaînes de caractères ******/
 /********* b. Concaténation ********************/
 
+
+$str1 = 'Bonjour';
+$str2 = 'Monde';
+$str3 = 'Hello';
+$str4 = 'World';
+
+$virgule = ',';
+$point_exclamation = '!';
+$point_interrogation = '?';
+$prenom = 'Jordan';     // Vous avez le droit de remplacer cette variable par votre prénom ;-)
+
+$str5 = "$str3 tout le monde $point_exclamation";
+$str6 = '$str1 $prenom, tu vas bien $point_interrogation';
+$str7 = 'Ca va' . $virgule . ' vous ' . $point_interrogation;
+
+/*  1)
+
+    Que va afficher un `echo $str5;` ?
+    => ?
+
+    Que va afficher un `echo $str6;` ?
+    => ?
+
+    Que va afficher un `echo $str7;` ?
+    => ?
+
+    Que va afficher un `echo $str8;` ?
+    => ?
+*/
+
+/*  2)
+
+    A partir de toutes les variables fournies, 
+    écrivez les phrases demandées avec la fonction `echo`, 
+    sans utiliser aucune autre chaîne de caractères
+    que les variables.
+*/
+
+$jmappelle = 'Je m\'appelle';
+// Ecrivez la phrase "Je m'appelle [prénom] !"
+// echo 
+
+// Ecrivez la phrase "Hello, World!"
+// echo 
+
+// Ecrivez la phrase "Hello tout le monde ! Je m'appelle [prénom] ! Ca va, vous ?"
+// Faites-le de 2 manières différentes.
+// echo 
+// echo 
