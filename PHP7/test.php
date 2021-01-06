f843b82044e09e1391251695930815292aaa6592
2alheure <jojo77.juv@live.fr>
2021-01-06 14:59:01 +0100

    test.php for WAMP installation testing

---
+++ PHP7/test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Hello, World!</title>
+</head>
+
+<body>
+    <p>
+        Ci-dessous devrait s'afficher "Hello, World!" :<br>
+        <?php echo 'Hello, World!'; ?>
+    </p>
+    <p>
+        Et ci-après devrait s'afficher une erreur :<br>
+        <?= $test; ?>
+    </p>
+</body>
+
+</html>
