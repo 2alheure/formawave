fe0eea097c90c667278ee25fe1d58984d2eb18fa
2alheure <jojo77.juv@live.fr>
2021-01-06 16:25:41 +0100

    Corrections of P2/C

--- PHP7/Corrections/P2/C/P2-C-I-a.php
+++ PHP7/Corrections/P2/C/P2-C-I-a.php
@@ -17,9 +17,39 @@ $nombre = 42;
 // grâce aux variables `masculin` et `nombre`
 
 // "Je suis marié(e) à Lucile. Ensemble, nous avons {nombre} enfant(s)."
+/**
+ * Ici nous allons utiliser "la méthode bourrin".
+ */
+if ($masculin) {
+    echo 'Je suis marié à Lucile.';
+} else {
+    echo 'Je suis mariée à Lucile.';
+}
 
+if ($nombre >= 2) {
+    echo "Ensemble, nous avons $nombre enfants.";
+} else {
+    echo "Ensemble, nous avons $nombre enfant.";
+}
 
 // "Il y a {nombre} anima[l / ux] dans ce zoo. C'est [peu / beaucoup] ! Je suis étonné(e) !"
+/**
+ * Ici on va un peu abréger en retirant les accolades inutiles.
+ */
+if ($nombre >= 2) echo "Il y a $nombre animaux dans ce zoo.";
+else "Il y a $nombre animal dans ce zoo.";
 
+if ($nombre > 40) echo 'C\'est beaucoup !';
+else echo 'C\'est peu !';
+
+if ($masculin) echo 'Je suis étonné !';
+else echo 'Je suis étonnée !';
 
 // "J'ai mangé {nombre} chocolat(s) de l'avent en {nombre - 10} jour(s), car j'en ai mangé plusieurs par jour car je suis gourmand(e)."
+/**
+ * Ici on va utiliser les ternaires
+ */
+echo 'J\'ai mangé ' . $nombre . ' chocolat' . ($nombre >= 2 ? 's' : '') . ' de l\'avent en '
+    . ($nombre - 10) . ' jour' . ($nombre - 10 >= 2 ? 's' : '') . ','
+    . 'car j\'en ai mangé plusieurs par jour '
+    . 'car je suis gourmand' . ($masculin ? '' : 'e') . '.';
