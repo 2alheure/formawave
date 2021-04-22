73ba2256dffb12918f6369d6da45d890a0a67908
2alheure <jojo77.juv@live.fr>
2021-04-22 12:27:29 +0200

    corrections partie 2

--- POO_MVC/POO/Corrections/1- Classes/Personnage.php
+++ POO_MVC/POO/Corrections/1- Classes/Personnage.php
@@ -14,31 +14,41 @@ require_once 'Cheval.php';
 
 class Personnage {
     public $pseudo;
-    public $pv;
     public $age;
     public $vivant = true;
-    public $force;
-    public $sagesse;
-    public $magie;
     public $guilde;
 
-    public function frappe(Personnage $quelqu_un) {
+    protected $pv = 100;
+    protected $force = 100;
+    protected $magie = 100;
+    protected $sagesse = 100;
+
+    // public function __construct(string $pseudo, int $age, int $pv = 0, int $force = 0, int $magie = 0, int $sagesse = 0) {
+    //     $this->pseudo = $pseudo;
+    //     $this->age = $age;
+    //     $this->pv = $pv;
+    //     $this->force = $force;
+    //     $this->magie = $magie;
+    //     $this->sagesse = $sagesse;
+    // }
+
+    public function frappe(Personnage $quelqu_un, int $bonus_frappe = 0) {
 
         if ($quelqu_un->guilde == $this->guilde)
             echo 'On tape pas les copains !';
 
-        if ($quelqu_un->vivant) {   // Si le personnage qu'on frappe est vivant
+        if ($quelqu_un->estVivant()) {   // Si le personnage qu'on frappe est vivant
             echo $this->pseudo . ' frappe ' . $quelqu_un->pseudo . '.';
 
             // Ma formule c'est : La force - 10 % de l'âge du personnage 
             // (arrondi à l'entier le plus proche)
-            $degats = round($this->force - $this->age * 0.1);
+            $degats = round($this->force - $this->age * 0.1) + $bonus_frappe;
 
             // Le "quelqu'un" subit nos dégats
             $quelqu_un->subitDegats($degats);
 
             // Si à présent il est mort
-            if (!$quelqu_un->vivant) {
+            if (!$quelqu_un->estVivant()) {
                 echo 'Ce coup a été fatal à ' . $quelqu_un->pseudo . '.';
             }
         } else {
@@ -50,8 +60,8 @@ class Personnage {
     }
 
     public function envoieSortSur(Personnage $quelqu_un) {
-        if ($quelqu_un->vivant) {   // Si le personnage qu'on frappe est vivant
-            echo $this->pseudo . ' frappe ' . $quelqu_un->pseudo . '.';
+        if ($quelqu_un->estVivant()) {   // Si le personnage qu'on frappe est vivant
+            echo $this->pseudo . ' envoie un sort sur ' . $quelqu_un->pseudo . '.';
 
             // Ma formule c'est : La magie + 10 % de l'âge du personnage 
             // (arrondi à l'entier le plus proche)
@@ -61,7 +71,7 @@ class Personnage {
             $quelqu_un->subitDegats($degats);
 
             // Si à présent il est mort
-            if (!$quelqu_un->vivant) {
+            if (!$quelqu_un->estVivant()) {
                 echo 'Ce sort a été fatal à ' . $quelqu_un->pseudo . '.';
             }
         } else {
@@ -69,6 +79,15 @@ class Personnage {
 
             if ($this->sagesse < 10) // Si le personnage n'est pas assez sage
                 echo $this->pseudo . ' s\'acharne sur le cadavre de ' . $quelqu_un->pseudo . '.';
+        }
+    }
+
+    public function estSoigne(int $soins) {
+        if ($this->estVivant()) {
+            echo $this->pseudo . ' a été soigné de ' . $soins . ' PV.';
+
+            // On "actualise" les PV du personnage
+            $this->pv += $soins;
         }
     }
 
@@ -96,7 +115,7 @@ class Personnage {
         $this->guilde = $guilde;
     }
 
-    private function initHP() {
-        $this->pv = rand(100, 500);
+    public function estVivant() {
+        return $this->vivant;
     }
 }
--- POO_MVC/POO/Corrections/2- Héritage/Mage.php
+++ POO_MVC/POO/Corrections/2- Héritage/Mage.php
@@ -3,3 +3,57 @@
 /**
  * Définissez la classe Mage, qui est un genre de Personnage
  */
+
+require_once '../1- Classes/Personnage.php';
+
+class Mage extends Personnage {
+    public $pv = 200;
+    public $force = 100;
+    public $magie = 500;
+    public $sagesse = 100;
+
+
+    public function soigne(Personnage $quelqu_un) {
+        if ($quelqu_un->estVivant()) {   // Si le personnage qu'on frappe est vivant
+            echo $this->pseudo . ' soigne ' . $quelqu_un->pseudo . '.';
+
+            // Ma formule c'est : 10 + 10 % de la magie du personnage 
+            // (arrondi à l'entier le plus proche)
+            $soins = round(10 + $this->magie * 0.1);
+
+            // Le "quelqu'un" est soigné
+            $quelqu_un->estSoigne($soins);
+        } else {
+            // Sinon
+
+            echo 'On ne peut soigner un cadavre';
+        }
+    }
+
+    public function bouleDeFeu(Personnage $quelqu_un) {
+        if ($quelqu_un->estVivant()) {   // Si le personnage qu'on frappe est vivant
+            echo $this->pseudo . ' envoie une boule de feu sur ' . $quelqu_un->pseudo . '.';
+
+            // Ma formule c'est : La magie + 10 % de l'âge du personnage 
+            // (arrondi à l'entier le plus proche)
+            $degats = round(150 + $this->magie + $this->age * 0.1);
+
+            // Le "quelqu'un" subit nos dégats
+            $quelqu_un->subitDegats($degats);
+
+            // Si à présent il est mort
+            if (!$quelqu_un->estVivant()) {
+                echo 'Ce sort a été fatal à ' . $quelqu_un->pseudo . '.';
+            }
+        } else {
+            // Sinon
+
+            if ($this->sagesse < 10) // Si le personnage n'est pas assez sage
+                echo $this->pseudo . ' s\'acharne sur le cadavre de ' . $quelqu_un->pseudo . '.';
+        }
+    }
+
+    public function frappe(Personnage $quelqu_un = null, int $bonus_frappe = 0) {
+        parent::frappe($quelqu_un, 10);
+    }
+}
