808d343887cbbb382922ab332b443dcd1e9f2a55
2alheure <jojo77.juv@live.fr>
2021-05-18 11:26:47 +0200

    Exos GET et POST

---
+++ PHP7/Exercices/P3/P3-C-I.php
@@ -0,0 +1,17 @@
+<?php
+
+/********* Partie 3 ***************************************************************/
+/********* C. Récupérer des données de requêtes (les superglobales) ***************/
+/********* I. GET *****************************************************************/
+
+/**
+ * Faire un formulaire (en GET)
+ * qui contient 2 champs : phrase et nombre
+ * 
+ * A la soumission du formulaire, 
+ * vous affichez $nombre fois la phrase $phrase.
+ * 
+ * (PAS DE JAVASCRIPT)
+ * 
+ * (P.S. : Vérifiez que le nombre n'est pas trop grand...)
+ */
---
+++ PHP7/Exercices/P3/P3-C-II.php
@@ -0,0 +1,17 @@
+<?php
+
+/********* Partie 3 ***************************************************************/
+/********* C. Récupérer des données de requêtes (les superglobales) ***************/
+/********* I. POST ****************************************************************/
+
+/**
+ * Faire un formulaire (en POST)
+ * qui contient 2 champs : identifiant et mot de passe.
+ * 
+ * A la soumission du formulaire,
+ * On vérifie que l'identifiant et le mot de passe 
+ * correspondent à ceux que l'on attend (stockés dans des variables).
+ * 
+ * S'ils correspondent, on affiche une page de succès, 
+ * sinon une page d'erreur.
+ */
