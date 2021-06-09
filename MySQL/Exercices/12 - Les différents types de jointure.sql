3af57621236329888be5b0f9d54283b8d6c887d9
2alheure <jojo77.juv@live.fr>
2021-06-09 12:51:35 +0200

    MySQL exercices 12 et 12bis

---
+++ MySQL/Exercices/12 - Les différents types de jointure.sql
@@ -0,0 +1,23 @@
+-- Ecrire les requêtes correspondant
+
+-- 1. Vous êtes futur parent,
+-- et vous souhaitez afficher toutes les combinaisons possibles entre nom et prenom
+-- pour votre enfant
+
+
+-- 2. Vous êtes un site de rencontre,
+-- et vous souhaitez lister toutes les combinaisons possibles entre 2 personnes
+
+
+-- 3. On précise le 2.
+-- en ne listant que les possibilités pour des personnes 
+-- qui sont d'un certain genre 
+-- et cherchent des personnes d'un certain genre
+
+
+-- 4. Lister tous les articles 
+-- et leur catégorie (stockée dans une table à part)
+
+
+-- 5. Lister tous les produits 
+-- et leur provenance (stockée dans une table à part)
---
+++ MySQL/Exercices/12bis - Les différents types de jointure.sql
@@ -0,0 +1,32 @@
+-- Ecrire les requêtes correspondant
+
+-- 1. Lister tous les utilisateurs 
+-- et, s'ils en ont un, leur rôle
+-- (le rôle est stocké dans une table à part)
+
+
+-- 2. Lister tous les véhicules
+-- la marque du véhicule (stockée dans une table à part)
+-- et, s'ils en ont un, leur propriétaire
+
+
+-- 3. Lister tous les articles,
+-- leur statut (stocké dans une table à part),
+-- leur auteur et le rôle de l'auteur
+-- (le rôle est stocké dans une table à part)
+
+
+-- 4. Lister tous les commentaires d'un article, 
+-- et leur auteur (aux commentaires comme à l'article)
+
+
+-- 5. Lister tous les produits du panier d'un utilisateur
+
+
+-- 6. Lister tous les livres que tous les utilisateurs possèdent
+
+
+-- Bonus. Lister tous les articles et le nombre de leurs commentaires
+
+
+-- Bonus 2. Récupérer le prix total du panier d'un utilisateur
