bfd574ad099cfec06eb14db6b5b848451249fd94
2alheure <jojo77.juv@live.fr>
2021-07-29 14:45:45 +0200

    12 + 12bis MySQL

--- MySQL/Corrections/12 - Les différents types de jointure.sql
+++ MySQL/Corrections/12 - Les différents types de jointure.sql
@@ -3,21 +3,30 @@
 -- 1. Vous êtes futur parent,
 -- et vous souhaitez afficher toutes les combinaisons possibles entre nom et prenom
 -- pour votre enfant
-
+SELECT * FROM prenom JOIN nom;
 
 -- 2. Vous êtes un site de rencontre,
 -- et vous souhaitez lister toutes les combinaisons possibles entre 2 personnes
-
+SELECT * FROM personne JOIN personne;
 
 -- 3. On précise le 2.
 -- en ne listant que les possibilités pour des personnes 
 -- qui sont d'un certain genre 
 -- et cherchent des personnes d'un certain genre
-
+SELECT * 
+FROM personne AS personne1 
+JOIN personne AS personne2 
+ON 
+	personne1.genre = personne2.recherche 
+	AND personne2.genre = personne1.recherche
+;
 
 -- 4. Lister tous les articles 
 -- et leur catégorie (stockée dans une table à part)
-
+SELECT * FROM article 
+JOIN categorie ON article.categorie = categorie.id;
 
 -- 5. Lister tous les produits 
 -- et leur provenance (stockée dans une table à part)
+SELECT * FROM produits 
+JOIN provenance ON produits.provenance = provenance.id;
--- MySQL/Corrections/12bis - Les différents types de jointure.sql
+++ MySQL/Corrections/12bis - Les différents types de jointure.sql
@@ -3,30 +3,58 @@
 -- 1. Lister tous les utilisateurs 
 -- et, s'ils en ont un, leur rôle
 -- (le rôle est stocké dans une table à part)
-
+SELECT * FROM utilisateurs 
+LEFT OUTER JOIN role ON utilisateurs.role = role.id;
 
 -- 2. Lister tous les véhicules
 -- la marque du véhicule (stockée dans une table à part)
 -- et, s'ils en ont un, leur propriétaire
-
+SELECT * FROM vehicules 
+JOIN marque ON vehicule.marque = marque.id
+LEFT OUTER JOIN proprietaire ON vehicule.proprietaire = proprietaire.id;
 
 -- 3. Lister tous les articles,
 -- leur statut (stocké dans une table à part),
 -- leur auteur et le rôle de l'auteur
 -- (le rôle est stocké dans une table à part)
-
+SELECT * FROM articles
+JOIN statut ON articles.statut = statut.id
+JOIN auteur ON articles.auteur = auteur.id
+JOIN role ON auteur.role = role.id;
 
 -- 4. Lister tous les commentaires d'un article, 
 -- et leur auteur (aux commentaires comme à l'article)
-
+SELECT * FROM commentaires
+JOIN article ON commentaire.article = article.id
+JOIN utilisateur AS auteur_de_comm ON commentaire.auteur = auteur_de_comm.id
+JOIN utilisateur AS auteur_de_article ON article.auteur = auteur_de_article.id
+WHERE article.id = 53;
 
 -- 5. Lister tous les produits du panier d'un utilisateur
-
+SELECT * FROM produits
+JOIN panier ON produits.id = panier.produit
+WHERE panier.user = 1337; -- 1337 = Exemple d'id utilisateur
 
 -- 6. Lister tous les livres que tous les utilisateurs possèdent
-
+SELECT livre.titre, utilisateur.nom FROM livre
+JOIN bibliotheque ON livre.id = bibliotheque.livre
+JOIN utilisateur ON bibliotheque.user = utilisateur.id;
 
 -- Bonus. Lister tous les articles et le nombre de leurs commentaires
-
+SELECT 
+	articles.*, 
+	COUNT(commentaires.id) AS nb_comm 
+FROM articles
+	JOIN commentaires ON commentaires.article = articles.id
+GROUP BY articles.id;
 
 -- Bonus 2. Récupérer le prix total du panier d'un utilisateur
+SELECT 
+	utilisateur.nom, 
+	utilisateur.prenom,
+	SUM(produits.prix * panier.quantite) AS montant_panier,
+	SUM(panier.quantite) AS nb_produit
+FROM produits
+	JOIN panier ON produits.id = panier.produit
+	JOIN utilisateur ON utilisateur.id = panier.user
+GROUP BY utilisateur.id HAVING utilisateur.id = 1337;
