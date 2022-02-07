7c48bb615e1f477f7d4df85f1e6d89bb063cfe13
2alheure <jojo77.juv@live.fr>
2022-02-07 10:07:51 +0100

    Update correction

--- PHP7/Corrections/P2/D/P2-D-III.php
+++ PHP7/Corrections/P2/D/P2-D-III.php
@@ -16,8 +16,7 @@
 */
 
 
-function nbJoursDepuis(int $jour, int $mois, int $annee)
-{
+function nbJoursDepuis(int $jour, int $mois, int $annee): int {
 	// On vérifie qu'on a eu une date valide
 	if (!checkdate($mois, $jour, $annee)) die('Une erreur s\'est produite.');
 
@@ -36,3 +35,74 @@ function nbJoursDepuis(int $jour, int $mois, int $annee)
 	// On renvoie le résultat
 	return $retour;
 }
+
+
+// Alternative
+/*
+function jours_ecoules_depuis(int $jour, int $mois, int $annee): int {
+	if (!checkdate($mois, $jour, $annee)) die('Date non valide');
+
+	$ajd_jour = date('z'); // Numéro du jour actuel
+	$ajd_annee = date('Y'); // Année actuelle
+
+	// Nombre d'années entière + le nombre de jours de l'année actuelle
+	$nb_jours = ($ajd_annee - $annee) * 365 + $ajd_jour;
+
+	// Retirer nombre de jours de l'année de départ
+	$nb_jours_depart = $jour;
+
+	switch ($mois) {
+		case 1:
+			$nb_jours_depart += 0; // Pas de mois entier passé
+			break;
+
+		case 2:
+			$nb_jours_depart += 31; // Juste le mois de janvier passé
+			break;
+
+		case 3:
+			$nb_jours_depart += 59; // Le mois de février passé
+			break;
+
+		case 4:
+			$nb_jours_depart += 90; // Le mois de mars passé
+			break;
+
+		case 5:
+			$nb_jours_depart += 120; // Le mois d'avril passé
+			break;
+
+		case 6:
+			$nb_jours_depart += 151; // Le mois de mai passé
+			break;
+
+		case 7:
+			$nb_jours_depart += 181; // Le mois de juin passé
+			break;
+
+		case 8:
+			$nb_jours_depart += 212; // Le mois de juillet passé
+			break;
+
+		case 9:
+			$nb_jours_depart += 243; // Le mois d'août passé
+			break;
+
+		case 10:
+			$nb_jours_depart += 273; // Le mois de septembre passé
+			break;
+
+		case 11:
+			$nb_jours_depart += 304; // Le mois d'octobre passé
+			break;
+
+		case 12:
+			$nb_jours_depart += 334; // Le mois de novembre passé
+			break;
+	}
+
+	$nb_jours -= $nb_jours_depart;
+
+	return $nb_jours;
+}
+*/
